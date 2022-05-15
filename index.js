232cba5a34885c56f2efedbbb2ed7c658435ad14
Lee Min Hung <leminhhungtabletennis@gmail.com>
2022-05-15 16:34:46 +0700

    add middleware to verify token

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
 
 const port = process.env.PORT || 3000;
 const SocketServices = require("./services/chat.service");
@@ -11,6 +15,16 @@ global._io = io;
 global.__basedir = __dirname;
 
 app.use(ChatRouter);
+
+// use middleware for request once
+global._io.use((socket, next) => {
+  const { token } = socket.handshake.headers;
+
+  // package JWT
+  if (token && token === "hellohung") return next();
+
+  next(new Error("Please login to chat with your friend"));
+});
 
 global._io.on("connection", SocketServices.connection);
 
